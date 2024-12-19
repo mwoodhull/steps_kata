4b4b656a1fbde8d875ab9bee6f63096288f018a8
mwoodhull <mwoodhull5@gmail.com>
2024-12-19 10:42:41 +0000

    completes first few tests

--- src/step.py
+++ src/step.py
@@ -1,2 +1,34 @@
 def step(x, y):
-    pass
+    
+    remainder = y - x
+
+    if remainder <= 2:
+        return remainder
+    
+    else:
+        return step_remainder(remainder)            
+
+def step_remainder(remainder):
+        count = 1
+        step_count = 1
+        remainder = remainder - step_count
+
+        if remainder <= 2:
+            count += 2
+            return count
+        else:
+             step_count += 1
+             remainder = remainder - step_count
+             count += 1
+             if remainder <= 2:
+                  step_count -= 1
+                  remainder = remainder - step_count
+                  count += 1
+                  if remainder == 0:
+                       return count
+                  else:
+                       remainder - step_count
+                       count += 1
+                       return count
+          
+
--- tests/test_step.py
+++ tests/test_step.py
@@ -1,9 +1,53 @@
 from src.step import step
 
+def test_x_and_y_are_the_same():
+
+    input1 = 4
+    input2 = 4
+    output = step(input1, input2)
+
+    assert output == 0
+
+def test_x_and_y_are_one_apart():
+
+    input1 = 11
+    input2 = 12
+    output = step(input1, input2)
+
+    assert output == 1   
+
 def test_x_and_y_that_are_two_apart():
+
     input1 = 11
     input2 = 13
-
     output = step(input1, input2)
 
     assert output == 2
+
+def test_x_and_y_are_3_apart():
+
+    input1 = 52
+    input2 = 55
+    output = step(input1, input2)
+
+    assert output == 3
+
+def test_x_and_y_are_more_than_3_apart():
+
+    input1 = 52
+    input2 = 57
+    output = step(input1, input2)
+
+    assert output == 4
+
+    input1 = 53
+    input2 = 57
+    output = step(input1, input2)
+
+    assert output == 3
+
+    input1 = 51
+    input2 = 57
+    output = step(input1, input2)
+
+    assert output == 5
